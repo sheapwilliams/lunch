41204ea31f021ef0b8f83a5483b99d38c3dbde85
Shea Williams <54335625+sheapwilliams@users.noreply.github.com>
2025-04-20 18:34:43 -0600

    add gunicorn driver

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+from app import app
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
+if __name__ == "__main__":
+    app.run() 
